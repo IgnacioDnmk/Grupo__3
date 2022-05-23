80bbd265d616d4d1ba4cea17d3ac2a0edb73ef4e
Nacho <ig.dinamarca@duocuc.cl>
2022-05-23 12:14:25 -0400

    arreglo de jquery para que funcione

--- JS/Formulario_trabajadores.js
+++ JS/Formulario_trabajadores.js
@@ -1,24 +1,10 @@
-$("#formLogin").validate({
-    rules:{
-      nombre: "required",
-      apellido: "required",
-      rut:"required",
-      email:"required"
-    },
-    messages: {
-      nombre: "Favor ingresar el nombre de trabajador",
-      apellido: "Favor ingresar el apellido del trabajador",
-      rut: "Favor ingresar el run del trabajador",
-      email: "Favor ingresar el run del trabajador"
-    }
-  });
 
 $("#formTrabajador").validate({
-    
+      rules:{
         nombre: {
           required: true,
           minlength: 5,
-          maxlength: 10
+          maxlength: 20
         },
 
         apellido: {
@@ -35,4 +21,11 @@ $("#formTrabajador").validate({
         required: true,
         email: true
         }
+      },
+      messages: {
+        nombre: "Favor ingresar el nombre de trabajador",
+        apellido: "Favor ingresar el apellido del trabajador",
+        rut: "Favor ingresar el run del trabajador",
+        email: "Favor ingresar el email"
+      }
 });
