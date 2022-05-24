5bf0401f91dc364be73691df98501c2b06f500e4
Erik <eri.torres@duocuc.cl>
2022-05-24 19:50:10 -0400

    logrado

--- JS/Untitled-2.js
+++ JS/Untitled-2.js
@@ -10,6 +10,8 @@ var contenedor_login_register = document.querySelector(".contenedor__login-regis
 var caja_trasera_login = document.querySelector(".caja__trasera-login");
 var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
+
+
     //FUNCIONES
 
 function anchoPage(){
@@ -62,3 +64,43 @@ anchoPage();
             caja_trasera_login.style.opacity = "1";
         }
 }
+
+$("#formLogin").validate({
+    rules:{
+      username: "required",
+      password: "required"
+    },
+    messages: {
+      username: "Favor ingresar el nombre de usuario",
+      password: "Favor ingresar la contraseña"
+    }
+  });
+
+$("#formRegistro").validate({
+  rules:{
+    username: {
+      required: true,
+      minlength: 5,
+      maxlength: 10
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    password1: {
+      required: true,
+      minlength: 10
+    },
+    password2:  {
+      required: true,
+      minlength: 10,
+      equalTo: "#password1"
+    },
+  },
+  messages: {
+    username: {
+      minlength: "Ingrese un Minimo de 5 caracteres",
+      maxlength: "Debe contener maximo 10 caracteres"
+    },
+  }
+});
